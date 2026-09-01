ab65219e065d0a98d36286e3c221606dff5f86b5
slovett0616 <slovett950@boyslatin.net>
2026-09-01 15:05:49 +0000

    first commit

---
+++ Unit 1/datatypes.py
@@ -0,0 +1,39 @@
+# STRING - characters that are wrapped within quotes. 
+# characters can be letters, numbers, and symbols.
+# you can use both single and double quotation 
+# marks for strings.
+
+"Syire lovett"
+
+"123 Main Street
+
+'this cost $100 dollars'
+            
+# the hashtag is a marker that makes anything written 
+# behind it invisible to the computer.     
+
+
+# INTEGERS - datatypes that are just whole numbers, or numbers 
+# without a decimal point. We dont have 
+# to add any special symbols. we can write the number 
+# as is.
+
+10
+20
+2000000
+
+# FLOSTS- A number with a decimal.
+
+
+20.99
+3.339
+0.23
+
+# BOOLEAN- a data type that can only be either 
+# true or false we do not use quotation marks for 
+# boolean values. the computer understand these words 
+# as something unique.
+
+False 
+True 
+
