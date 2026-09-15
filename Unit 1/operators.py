3230067cd1c329b93d87903838c496ea0a6a16b8
slovett0616 <slovett950@boyslatin.net>
2026-09-15 15:11:11 +0000

    OperatorsActivity

--- Unit 1/operators.py
+++ Unit 1/operators.py
@@ -18,3 +18,64 @@ print(3 + 3) # addition operator
 print(3.9 - 3) # subtraction operator
 print(12 / 3) # division operator
 print(3 * 4) # multiplication operator 
+
+# Assignment Operators- Exclusively used to
+# assign values to variables.
+# think key / value pairings
+
+# We use a single equal sign to represent the assignment
+# operator
+name = "Syire Lovett"
+grade = 10
+school = True
+
+# Arithmetic Operators- Used on numerical
+# data types to perform calculations.
+# integers (whole numbers) and floats (decimal numbers)
+
+# print is a function that lets us show code
+# in the terminal
+print(3 + 3) # addition operator
+print(3.9 - 3) # subtraction operator
+print(12 / 3) # division operator
+print(3 * 4) # multiplication operator
+
+# Comparison Operators - Set of symbols used
+# to assess if data is the same or different and
+# how they differ
+
+print(10 > 1) # greater than operator
+print(2000 < 100) # less than operator
+
+# 2 equal signs compare if something is the SAME
+print("book" == "Book") # same as (false)
+print("2" == 2) # same as (false)
+print(2.0 == 2) # same as (true)
+
+# not equal is written with != 
+# this is to check and filter for values that are not
+# the same
+
+# side note - exclamation ALWAYS means NOT in programming
+
+print( 200 != 300) # True- these are not the same
+print(300 != 300) # False- these are the same
+
+# logical operators- compares 2 conditions to check if
+# they are true or false
+# conditions = other operators
+# instead of symbols, we represent these with words:
+# and, or, not
+
+# AND - checks if 2 conditions are true. if yes, the final
+# result is true
+print(3 > 1 and 100 > 50) # this would come out to be true
+
+# OR- checks if only 1 condition is true. if yes,
+# the final result will be true
+print( 3 > 1 or 100 == 50)
+
+# Not - the "opposite day" operator. it will reverse the
+# result of the logical operators
+print(not(3 > 1 and 100 > 50))
+# this would come out to be false
---
+++ Unit 1/operatorsActivity.py
@@ -0,0 +1,15 @@
+# Scenario Activity: what operator family is being used and what operator symbol are you using?
+
+# Scenario 1.) Arithmetic Operator because its calculating balances
+# Scenario 2.) Logical Operator because both conditions need to be true
+# Scenario 3.) Logical Operator because they are checking if the contact numbers work
+# Scenario 4.) Comparisson Operator because its checking if there was 100$ spent or not
+# Scenario 5.) Arithmetic Operator because they are calculating a 7% tax to 200$ shoes
+
+# Assignment Scenarios:
+
+
+print(200== 100)  # False
+print(13 > 18) # False 
+print("Coding 1"== "coding1") # False
+print(0 != 0) # False
